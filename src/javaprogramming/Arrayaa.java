5e32de3fab76500fec45a57172fddb7acb0a5229
yarrisw <yarriswamy1@gmail.com>
2022-12-14 18:41:02 +0530

    pushing the code

---
+++ src/javaprogramming/Arrayaa.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+public class Arrayaa 
+{
+	static void array(int[]arr)
+	{zz
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+"");
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of the array");
+		int size=sc.nextInt();
+		int[] arr =new int[size];
+		System.out.println("Enter array elements");
+		for (int i=0;i<arr.length; i++);
+	}
+}
+array(arr);
+}
